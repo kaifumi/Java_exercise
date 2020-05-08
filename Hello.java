cd92520f9d0b5afb76a688b3e79c21d00866182f
kaifumi <kaifumi1001@gmail.com>
2020-05-08 11:27:15 +0900

    delete_lesson_folder

---
+++ Hello.java
@@ -0,0 +1,12 @@
+// 画面への出力を行うプログラム
+
+class Hello{
+
+  public static void main(String[] args) {
+    System.out.println("初めてのJavaaaaaプログラム。");
+    System.out.println("画面に出力しています。");
+  }
+}
+
+// フォルダ直下でjavacしたクラスファイルでないと
+// コンパイル後に実行できない
